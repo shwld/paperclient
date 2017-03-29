fcfd05d9614e5a00735ae9d1bdcc6f61985983cb
shwld@outlook.com <shwld@outlook.com>
2017-03-30 08:54:59 +0900

    v0.0.2

--- build.sh
+++ build.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-electron-packager . PaperClient --overwrite --platform=darwin --arch=x64 --version=1.3.3 --asar=true --build-version=0.0.1 --icon ./paperclient.icns
+PKG_VERSION=`node -pe 'require("./dist/package.json").version'`
+
+echo "version: ${PKG_VERSION}"
+
+electron-packager ./dist "PaperClient" \
+  --overwrite \
+  --platform=darwin \
+  --arch=x64 \
+  --version=1.4.13 \
+  --build-version=${PKG_VERSION} \
+  --icon=./icon.icns \
+  --app-bundle-id=net.shwld.PaperClient \
+  --extend-info="extend.plist" \
+  --helper-bundle-id=PaperClient \
+  --protocol=paper \
+  --protocol-name=paper
+#   --osx-sign=true
+
+zip -r -y ./"PaperClient-"${PKG_VERSION}-darwin-x64.zip ./"PaperClient"-darwin-x64/"PaperClient".app
